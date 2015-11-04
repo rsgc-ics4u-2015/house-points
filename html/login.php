14127336e488c135337a59b141c1237688b76c8d
rsgc_hill_r <rhill@rsgc.on.ca>
2015-11-04 17:22:53 +0000

    making php subfolder

--- html/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-//$connection = mysqli_connect($host, $user, $pass, $db, $port) or die(mysql_error());
-  // And now perform simple query – make sure it's working
- // $query = "SELECT * FROM code;";
-  //$result = mysqli_query($connection, $query);
-  
-  if()
-  
-?>
